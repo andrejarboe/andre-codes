b856cd7771b9d8df7b6de71a7fa27680cb3324d0
Andre Jarboe <andrejarboe@gmail.com>
2022-01-25 18:32:25 -0500

    add about page

---
+++ client/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainLayout from '../components/Layouts/MainLayout';
+
+export default function about() {
+  return (
+    <div>
+      <MainLayout>
+        <h1>about</h1>
+      </MainLayout>
+    </div>
+  );
+}
